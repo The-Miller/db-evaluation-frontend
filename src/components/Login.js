7fadfe969d4c961dd4cc231c242e61b49479d960
The-Miller <niang08boubacar@gmail.com>
2025-03-20 11:08:35 +0000

    Suppression du préfixe /api dans les appels API (géré par api.js)

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,7 +11,7 @@ function Login({ onLogin }) {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await api.post('/api/login', { email, password });
+      const response = await api.post('/login', { email, password });
       localStorage.setItem('token', response.data.token);
       onLogin(response.data.user);
     } catch (error) {
